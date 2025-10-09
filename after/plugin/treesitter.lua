488a73249fc8485cb1206fa1241acb8ba6520cda
Ismail Fahmy <ismailmn05@gmail.com>
2025-10-09 19:04:29 +0200

    add treesitter for devops languages

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,17 +1,20 @@
 local status_ok, configs = pcall(require, "nvim-treesitter.configs")
-if not status_ok then
-	return
+if not status_ok then return
 
 configs.setup {
     auto_install = true,
-  ensure_installed = { "bash", "c", "javascript", "lua", "python", "css", "rust", "java"}, -- one of "all" or a list of languages
+    ensure_installed = { "bash", "c", "javascript", "lua", "python", "css", "rust", "java", "yaml", "toml", "terraform", }, -- one of "all" or a list of languages
 	ignore_install = { "phpdoc" }, -- List of parsers to ignore installing
+
 	highlight = {
 		enable = true, -- false will disable the whole extension
 		disable = { "css" }, -- list of language that will be disabled
 	},
+
 	autopairs = {
 		enable = true,
 	},
 	indent = { enable = true, disable = { "python", "css" } },
-}end
+}
+
+end
